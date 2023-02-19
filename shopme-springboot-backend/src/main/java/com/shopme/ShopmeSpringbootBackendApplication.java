33b5282f038c300f102ae781a3010b19481cf67a
Anurag Kumar <anuragkr41@gmail.com>
2023-02-19 22:55:49 +0530

    3nd commit

--- shopme-springboot-backend/src/main/java/com/shopme/ShopmeSpringbootBackendApplication.java
+++ shopme-springboot-backend/src/main/java/com/shopme/ShopmeSpringbootBackendApplication.java
@@ -9,5 +9,6 @@ public class ShopmeSpringbootBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShopmeSpringbootBackendApplication.class, args);
     }
+    //added
 
 }
